f14d8f1ecf338222f15334706868e16c70b9b41f
PatrickAbonado <91986188+PatrickAbonado@users.noreply.github.com>
2023-12-02 10:03:46 -0500

    -created a runnable jar file -took out login page io error message

--- src/main/java/capstone/schedulemanager/Driver.java
+++ src/main/java/capstone/schedulemanager/Driver.java
@@ -23,7 +23,6 @@ public class Driver extends Application {
         }
         catch(IOException ioe){
             ioe.printStackTrace();
-            helpers.pageLoadErrMsg();
         }
     }
 
---
+++ src/main/java/capstone/schedulemanager/Schedule_Manager_JAR_Run.java
@@ -0,0 +1,11 @@
+package capstone.schedulemanager;
+
+public class Schedule_Manager_JAR_Run {
+
+    public static void main(String[] args) {
+
+        Driver.main(args);
+    }
+
+
+}
